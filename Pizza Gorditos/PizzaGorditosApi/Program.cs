47f38188e5700c07531de2784896161296bb072c
valeria-srva <saraviavaleria05@gmail.com>
2026-06-01 08:24:27 -0600

    Update Program.cs

--- Pizza Gorditos/PizzaGorditosApi/Program.cs
+++ Pizza Gorditos/PizzaGorditosApi/Program.cs
@@ -34,5 +34,10 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.MapControllers();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PizzaGorditosDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
