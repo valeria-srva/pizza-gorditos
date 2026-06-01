71f08db8462293131c8884aaf68525a3ec1e0c21
valeria-srva <saraviavaleria05@gmail.com>
2026-06-01 07:32:53 -0600

    Add files via upload

---
+++ Pizza Gorditos/PizzaGorditosApi/20260601132550_CrearBasePizzaGorditos.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PizzaGorditosApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class CrearBasePizzaGorditos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pizzas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Sku = table.Column<string>(type: "TEXT", nullable: false),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: false),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false),
+                    PrecioPersonal = table.Column<decimal>(type: "TEXT", nullable: false),
+                    PrecioMediana = table.Column<decimal>(type: "TEXT", nullable: false),
+                    PrecioFamiliar = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pizzas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: false),
+                    Telefono = table.Column<string>(type: "TEXT", nullable: false),
+                    Direccion = table.Column<string>(type: "TEXT", nullable: false),
+                    Departamento = table.Column<string>(type: "TEXT", nullable: false),
+                    Municipio = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pedidos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CodigoPedido = table.Column<string>(type: "TEXT", nullable: false),
+                    FechaPedido = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UsuarioId = table.Column<int>(type: "INTEGER", nullable: false),
+                    MetodoPago = table.Column<string>(type: "TEXT", nullable: false),
+                    Subtotal = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Descuento = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Envio = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Total = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Paso = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedidos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pedidos_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DetallesPedido",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PedidoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    PizzaId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Tamano = table.Column<string>(type: "TEXT", nullable: false),
+                    Cantidad = table.Column<int>(type: "INTEGER", nullable: false),
+                    PrecioUnitario = table.Column<decimal>(type: "TEXT", nullable: false),
+                    SubtotalLinea = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DetallesPedido", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DetallesPedido_Pedidos_PedidoId",
+                        column: x => x.PedidoId,
+                        principalTable: "Pedidos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DetallesPedido_Pizzas_PizzaId",
+                        column: x => x.PizzaId,
+                        principalTable: "Pizzas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Pizzas",
+                columns: new[] { "Id", "Descripcion", "Nombre", "PrecioFamiliar", "PrecioMediana", "PrecioPersonal", "Sku" },
+                values: new object[,]
+                {
+                    { 1, "Pepperoni y queso extra", "Pepperoni", 9m, 7m, 4m, "pep" },
+                    { 2, "Tomate, mozzarella, albahaca", "Margarita", 10m, 8m, 5m, "mar" },
+                    { 3, "Mozzarella, gorgonzola, parmesano, ricotta", "4 Quesos", 11m, 9m, 6m, "4q" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetallesPedido_PedidoId",
+                table: "DetallesPedido",
+                column: "PedidoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetallesPedido_PizzaId",
+                table: "DetallesPedido",
+                column: "PizzaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedidos_UsuarioId",
+                table: "Pedidos",
+                column: "UsuarioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DetallesPedido");
+
+            migrationBuilder.DropTable(
+                name: "Pedidos");
+
+            migrationBuilder.DropTable(
+                name: "Pizzas");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
