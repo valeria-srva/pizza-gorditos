425f83be803092f3fa991a82275e6fb6a0593611
valeria-srva <saraviavaleria05@gmail.com>
2026-06-01 08:27:50 -0600

    Update Program.cs

--- Pizza Gorditos/PizzaGorditosApi/Program.cs
+++ Pizza Gorditos/PizzaGorditosApi/Program.cs
@@ -38,6 +38,16 @@ using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<PizzaGorditosDbContext>();
     db.Database.Migrate();
+
+    if (!db.Pizzas.Any())
+    {
+        db.Pizzas.AddRange(
+            new PizzaGorditosApi.Models.Pizza { Sku = "pep", Nombre = "Pepperoni", Descripcion = "Pepperoni y queso extra", PrecioPersonal = 4, PrecioMediana = 7, PrecioFamiliar = 9 },
+            new PizzaGorditosApi.Models.Pizza { Sku = "mar", Nombre = "Margarita", Descripcion = "Tomate, mozzarella, albahaca", PrecioPersonal = 5, PrecioMediana = 8, PrecioFamiliar = 10 },
+            new PizzaGorditosApi.Models.Pizza { Sku = "4q", Nombre = "4 Quesos", Descripcion = "Mozzarella, gorgonzola, parmesano, ricotta", PrecioPersonal = 6, PrecioMediana = 9, PrecioFamiliar = 11 }
+        );
+        db.SaveChanges();
+    }
 }
 
 app.Run();
