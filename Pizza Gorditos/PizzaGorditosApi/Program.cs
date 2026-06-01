3bbabe5c62be329457ec6311e7d52e09c6069076
valeria-srva <saraviavaleria05@gmail.com>
2026-06-01 08:48:14 -0600

    Update Program.cs

--- Pizza Gorditos/PizzaGorditosApi/Program.cs
+++ Pizza Gorditos/PizzaGorditosApi/Program.cs
@@ -23,16 +23,14 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<PizzaGorditosDbContext>();
+    db.Database.EnsureDeleted();
     db.Database.Migrate();
-    if (!db.Pizzas.Any())
-    {
-        db.Pizzas.AddRange(
-            new PizzaGorditosApi.Models.Pizza { Sku = "pep", Nombre = "Pepperoni", Descripcion = "Pepperoni y queso extra", PrecioPersonal = 4, PrecioMediana = 7, PrecioFamiliar = 9 },
-            new PizzaGorditosApi.Models.Pizza { Sku = "mar", Nombre = "Margarita", Descripcion = "Tomate, mozzarella, albahaca", PrecioPersonal = 5, PrecioMediana = 8, PrecioFamiliar = 10 },
-            new PizzaGorditosApi.Models.Pizza { Sku = "4q", Nombre = "4 Quesos", Descripcion = "Mozzarella, gorgonzola, parmesano, ricotta", PrecioPersonal = 6, PrecioMediana = 9, PrecioFamiliar = 11 }
-        );
-        db.SaveChanges();
-    }
+    db.Pizzas.AddRange(
+        new PizzaGorditosApi.Models.Pizza { Sku = "pep", Nombre = "Pepperoni", Descripcion = "Pepperoni y queso extra", PrecioPersonal = 4, PrecioMediana = 7, PrecioFamiliar = 9 },
+        new PizzaGorditosApi.Models.Pizza { Sku = "mar", Nombre = "Margarita", Descripcion = "Tomate, mozzarella, albahaca", PrecioPersonal = 5, PrecioMediana = 8, PrecioFamiliar = 10 },
+        new PizzaGorditosApi.Models.Pizza { Sku = "4q", Nombre = "4 Quesos", Descripcion = "Mozzarella, gorgonzola, parmesano, ricotta", PrecioPersonal = 6, PrecioMediana = 9, PrecioFamiliar = 11 }
+    );
+    db.SaveChanges();
 }
 
 app.UseCors("PermitirFrontend");
