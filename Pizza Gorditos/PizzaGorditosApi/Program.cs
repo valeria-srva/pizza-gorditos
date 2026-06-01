dec40bcc3c7a4de27347682a497dcda6e9c4cc97
valeria-srva <saraviavaleria05@gmail.com>
2026-06-01 11:24:26 -0600

    Update Program.cs

--- Pizza Gorditos/PizzaGorditosApi/Program.cs
+++ Pizza Gorditos/PizzaGorditosApi/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using PizzaGorditosApi.Data;
-using PizzaGorditosApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,23 +24,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Crea las tablas y siembra las pizzas iniciales al arrancar
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<PizzaGorditosDbContext>();
-    db.Database.ExecuteSqlRaw("DROP TABLE IF EXISTS \"__EFMigrationsHistory\";");
-    db.Database.EnsureCreated();
-
-    if (!db.Pizzas.Any())
-    {
-        db.Pizzas.AddRange(
-            new Pizza { Sku = "pep", Nombre = "Pepperoni", Descripcion = "Pepperoni y queso extra", PrecioPersonal = 4, PrecioMediana = 7, PrecioFamiliar = 9 },
-            new Pizza { Sku = "mar", Nombre = "Margarita", Descripcion = "Tomate, mozzarella, albahaca", PrecioPersonal = 5, PrecioMediana = 8, PrecioFamiliar = 10 },
-            new Pizza { Sku = "4q", Nombre = "4 Quesos", Descripcion = "Mozzarella, gorgonzola, parmesano, ricotta", PrecioPersonal = 6, PrecioMediana = 9, PrecioFamiliar = 11 }
-        );
-        db.SaveChanges();
-    }
-}
+// Las tablas se crean manualmente en Supabase
 
 app.UseCors("PermitirFrontend");
 
