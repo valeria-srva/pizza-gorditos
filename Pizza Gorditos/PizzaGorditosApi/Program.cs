b73f723cb2648a68fec2079d863c77a6f898a42d
valeria-srva <saraviavaleria05@gmail.com>
2026-06-01 11:18:35 -0600

    Update Program.cs

--- Pizza Gorditos/PizzaGorditosApi/Program.cs
+++ Pizza Gorditos/PizzaGorditosApi/Program.cs
@@ -29,7 +29,8 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<PizzaGorditosDbContext>();
-    db.Database.Migrate();
+    db.Database.ExecuteSqlRaw("DROP TABLE IF EXISTS \"__EFMigrationsHistory\";");
+    db.Database.EnsureCreated();
 
     if (!db.Pizzas.Any())
     {
